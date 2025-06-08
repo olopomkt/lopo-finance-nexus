ee5ffcc3725fe87edf57fa60225682354d887684
olopomkt <lopomktoficial@gmail.com>
2025-06-08 14:15:21 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,17 +4,20 @@ import { StarsBackground } from '@/components/ui/stars';
 export const Header = () => {
   return (
     <div className="relative">
-      <StarsBackground className="relative flex w-full h-[650px] justify-center items-center overflow-hidden">
-        
-        {/* Buraco negro centralizado, no fundo */}
+      {/* StarsBackground é o cenário */}
+      <div className="relative w-full h-[650px] flex justify-center items-center bg-black">
+        {/* Fundo de estrelas */}
+        <StarsBackground className="absolute inset-0 z-0" />
+
+        {/* Imagem do buraco negro */}
         <img 
           src="/lovable-uploads/0f9579c4-e6ad-4aab-800e-e273a39c0b37.png" 
           alt="Buraco Negro" 
-          className="absolute bottom-0 left-1/2 transform -translate-x-1/2 scale-[2] z-0 pointer-events-none"
+          className="absolute bottom-0 left-1/2 transform -translate-x-1/2 scale-[2] z-10 pointer-events-none"
         />
 
-        {/* Título centralizado sobre o buraco negro */}
-        <div className="relative z-10 flex flex-col w-full h-full justify-center items-center" style={{ marginTop: '-50px' }}>
+        {/* Conteúdo centralizado acima do buraco negro */}
+        <div className="relative z-20 flex flex-col items-center" style={{ marginTop: '-50px' }}>
           <div className="font-florence font-normal text-7xl text-[#444444] text-center">
             FINANCE CONTROL
           </div>
@@ -22,7 +25,7 @@ export const Header = () => {
             MARLON LOPO
           </div>
         </div>
-      </StarsBackground>
+      </div>
     </div>
   );
 };
